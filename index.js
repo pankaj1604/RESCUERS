6cc37fc0ae54ea673f06155b4bb745a8e4f8e207
Pankaj Kumar <dev.pankajkr7@gmail.com>
2026-04-12 11:04:32 +0530

    Added Health Profile Page

---
+++ index.js
@@ -0,0 +1,27 @@
+// Hero Sign In Options
+let hero_sign_in_btn = document.getElementById('hero-sign-in-btn');
+let sign_in_opt = document.getElementById('sign-in-opt');
+
+hero_sign_in_btn.addEventListener('mouseover', ()=>{
+    sign_in_opt.style.visibility = "visible";
+    sign_in_opt.style.bottom = "-210%"
+})
+
+hero_sign_in_btn.addEventListener('mouseout', ()=>{
+    sign_in_opt.style.visibility = "hidden";
+    sign_in_opt.style.bottom = "-250%"
+})
+
+// Hero Sign Up Options
+let hero_sign_up_btn = document.getElementById('hero-sign-up-btn');
+let sign_up_opt = document.getElementById('sign-up-opt');
+
+hero_sign_up_btn.addEventListener('mouseover', ()=>{
+    sign_up_opt.style.visibility = "visible";
+    sign_up_opt.style.bottom = "-210%"
+})
+
+hero_sign_up_btn.addEventListener('mouseout', ()=>{
+    sign_up_opt.style.visibility = "hidden";
+    sign_up_opt.style.bottom = "-250%"
+})
